e19c6030ad1cad124919e58eeca7d0d7034c7b66
punitjha <punitjha111@gmail.com>
2017-06-09 21:13:46 -0500

    the beginings of the dream Hartree-Fock Project

---
+++ hartree.cpp
@@ -0,0 +1,26 @@
+//The mass header file
+
+#include "mass.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <vector>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
+#include <armadillo>
+#include <complex>
+
+#include <array>   // to be able to use this library complie using this g++ -std=c++0x FileNames
+
+using namespace std;
+
+int main()
+{
+}
